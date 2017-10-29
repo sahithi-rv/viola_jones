12ab7dcf164da9c7d73849596b8278609133f216
Sahithi R V <sahithi.rv1@gmail.com>
2017-10-29 12:13:19 +0530

    int to hex script

---
+++ filetohex.py
@@ -0,0 +1,20 @@
+import numpy as np
+import sys
+
+filein = sys.argv[1];
+fileout = sys.argv[2];
+
+l = []
+
+with open(filein,'r') as f:
+	for line in f:
+		k = int(line.strip());
+		h = hex(k)
+		h = h.split('x')[1]
+		l.append(h.strip())
+
+
+fd = open(fileout,'w')
+for i in l:
+	print >> fd, i
+
